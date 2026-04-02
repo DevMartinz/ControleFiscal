d9edb79742b056f5ff9b95c56fbfcde972ffca27
Dev_Martinz <Martiinz.742@gmail.com>
2026-04-01 23:33:15 -0300

    Correcao Header Ano

--- src/components/TabelaFiscal.jsx
+++ src/components/TabelaFiscal.jsx
@@ -355,7 +355,7 @@ const LinhaDaTabela = React.memo(
 										d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"
 									/>
 								</svg>
-								Sem Movimentos Nesta Competência
+								Sem Movimentos
 							</span>
 						</div>
 					</td>
